9c9b0b8681b33569c8aef0684b5e0d071b2cc33d
Matěj Laitl <matej@laitl.cz>
2021-02-23 14:33:31 +0100

    Provide "audio echo effect" example (#28)
    * Provide "audio echo effect" example
    
    This is somewhat overlapping with the media_pipeline example, but it actually
    works, and was fun to write and test. :) I wanted to get some hands-on
    experience, and also try circular actor dependencies.
    
    Provided you have PulseAudio installed, run it as:
    
    parec --latency=2940 | ./target/debug/examples/echo | pacat --latency=2940
    
    BEWARE, running is prone to create loud audio feedback. Headphones ease that.
    
    The simple_logger already used in media_pipeline unconditionally writes to
    stdout, which garbles audio output, so I switched to env_logger.
    
    Potential shortcomings/follow-ups:
    - It would be nice to show how to perform clean shutdown.
    - Not sure if even shorter example would be desired?
    
    Fixes #4.
    
    * echo example: set higher capacity to show more of the API
    * echo example: address review comments
    * echo example: introduce silence_chunk() to prevent duplication

---
+++ examples/echo.rs
@@ -0,0 +1,253 @@
+//! A simple actor system that implements a working "echo" audio effect.
+//!
+//! Provided you have PulseAudio installed, run it as:
+//!
+//! parec --latency=2940 | ./target/debug/examples/echo | pacat --latency=2940
+//!
+//! BEWARE, running is prone to create loud audio feedback. Headphones ease that.
+//!
+//!            +-------+                   +--------+
+//! stdin +----> Input +-+             +---> Output +---> stdout
+//!            +-------+ |   +-------+ |   +--------+
+//!                      +---> Mixer +-+
+//!                   +------>       +--------+
+//!                   |      +-------+        |
+//!               +--------+              +---v---+
+//!               | Damper <--------------+ Delay |
+//!               +--------+              +-------+
+
+use actor::{Actor, Addr, Context, NoopMetricsHandler, Recipient, System};
+use env_logger::Env;
+use failure::Error;
+use log::trace;
+use std::{
+    convert::TryInto,
+    io::{stdin, stdout, BufWriter, Read, Write},
+    iter::repeat,
+    sync::Arc,
+};
+
+/// One audio sample. Defaults of pacat are --format=s16ne (signed 16bit) and --chanels=2 (stereo).
+type Sample = [i16; 2];
+
+/// Number of bytes one encoded sample has. This is same as the size of its memory representation.
+const SAMPLE_BYTES: usize = std::mem::size_of::<Sample>();
+
+/// Number of samples per chunk. pacat defaults to --rate=44100, let's go for 60 chunks per second.
+/// For best results, set --latency=<CHUNK_SAMPLES>*<SAMPLE_BYTES> for both parec and pacat.
+const CHUNK_SAMPLES: usize = 44100 / 60;
+
+/// A chunk of audio, an array of samples.
+type Chunk = Arc<[Sample; CHUNK_SAMPLES]>;
+
+/// Number of chunks the "Delay" actor (effect) has.
+const DELAY_CHUNKS: usize = 60;
+
+/// Dummy trigger for [`Input`] to read next chunk.
+struct ReadNext;
+
+fn silence_chunk() -> Chunk {
+    Arc::new([[0i16; 2]; CHUNK_SAMPLES])
+}
+
+/// Actor to read and decode input stream (stdin) and produce sound chunks.
+struct Input {
+    next: Recipient<MixerInput>,
+}
+
+impl Actor for Input {
+    type Error = Error;
+    type Message = ReadNext;
+
+    fn name() -> &'static str {
+        "Input"
+    }
+
+    fn handle(&mut self, context: &Context<Self>, _message: ReadNext) -> Result<(), Self::Error> {
+        // Read data from stdin and decode into correct format.
+        let mut bytes = [0u8; CHUNK_SAMPLES * SAMPLE_BYTES];
+        stdin().read_exact(&mut bytes)?;
+        let chunk_slice: Arc<[Sample]> = bytes
+            .chunks(SAMPLE_BYTES)
+            // default PulseAudio format is s16ne, signed 16 bit native endian.
+            .map(|b| [i16::from_ne_bytes([b[0], b[1]]), i16::from_ne_bytes([b[2], b[3]])])
+            .collect();
+        let chunk: Chunk = chunk_slice.try_into().expect("sample count is correct");
+        trace!("[Input] decoded chunk: {:?}...", &chunk[..5]);
+
+        // Send the parsed chunk to the next actor.
+        self.next.try_send(MixerInput::Dry(chunk))?;
+
+        // Trigger a loop to read the next chunk.
+        context.myself.try_send(ReadNext).map_err(Error::from)
+    }
+}
+
+/// Actor to encode and write sound chunks to output stream (stdout).
+struct Output;
+
+impl Actor for Output {
+    type Error = Error;
+    type Message = Chunk;
+
+    fn name() -> &'static str {
+        "Output"
+    }
+
+    fn handle(&mut self, _context: &Context<Self>, message: Chunk) -> Result<(), Self::Error> {
+        let mut buffered_stdout = BufWriter::new(stdout());
+        for sample in message.iter() {
+            for bytes in sample.iter().map(|s| s.to_ne_bytes()) {
+                buffered_stdout.write_all(&bytes)?;
+            }
+        }
+        buffered_stdout.flush().map_err(Error::from)
+    }
+}
+
+/// A chunk that knows whether it is dry or wet.
+enum MixerInput {
+    /// The original signal.
+    Dry(Chunk),
+    /// Signal from an effect.
+    Wet(Chunk),
+}
+
+/// Audio mixer actor. Mixes 2 inputs (dry, wet) together, provides 2 equal outputs.
+struct Mixer {
+    out_1: Recipient<Chunk>,
+    out_2: Recipient<Chunk>,
+    dry_buffer: Option<Chunk>,
+    wet_buffer: Option<Chunk>,
+}
+
+impl Mixer {
+    fn new(out_1: Recipient<Chunk>, out_2: Recipient<Chunk>) -> Self {
+        // Start with buffers filled, so that output is produced right for the first message.
+        Self { out_1, out_2, dry_buffer: Some(silence_chunk()), wet_buffer: Some(silence_chunk()) }
+    }
+}
+
+impl Actor for Mixer {
+    type Error = Error;
+    type Message = MixerInput;
+
+    fn name() -> &'static str {
+        "Mixer"
+    }
+
+    fn handle(&mut self, _context: &Context<Self>, message: MixerInput) -> Result<(), Error> {
+        // Naive implementation that simply overwrites on overflow.
+        match message {
+            MixerInput::Dry(chunk) => self.dry_buffer = Some(chunk),
+            MixerInput::Wet(chunk) => self.wet_buffer = Some(chunk),
+        }
+
+        // if both buffers are full, mix them and send out.
+        if let (Some(dry), Some(wet)) = (&self.dry_buffer, &self.wet_buffer) {
+            let mixed_slice: Arc<[Sample]> = dry
+                .iter()
+                .zip(wet.iter())
+                .map(|(a, b)| [a[0].saturating_add(b[0]), a[1].saturating_add(b[1])])
+                .collect();
+            let mixed: Chunk = mixed_slice.try_into().expect("sample count is correct");
+
+            self.out_1.try_send(Arc::clone(&mixed))?;
+            self.out_2.try_send(mixed)?;
+
+            self.dry_buffer = None;
+            self.wet_buffer = None;
+        }
+        Ok(())
+    }
+}
+
+/// Delay audio effect actor. Technically just a fixed circular buffer.
+struct Delay {
+    next: Recipient<Chunk>,
+    buffer: Vec<Chunk>,
+    index: usize,
+}
+
+impl Delay {
+    fn new(next: Recipient<Chunk>) -> Self {
+        let buffer: Vec<Chunk> = repeat(silence_chunk()).take(DELAY_CHUNKS).collect();
+        Self { next, buffer, index: 0 }
+    }
+}
+
+impl Actor for Delay {
+    type Error = Error;
+    type Message = Chunk;
+
+    fn name() -> &'static str {
+        "Delay"
+    }
+
+    fn handle(&mut self, _context: &Context<Self>, message: Chunk) -> Result<(), Error> {
+        self.buffer[self.index] = message;
+
+        // Advance index, reset to zero on overflow.
+        self.index = (self.index + 1) % self.buffer.len();
+
+        // Send out the least recent chunk.
+        self.next.try_send(Arc::clone(&self.buffer[self.index])).map_err(Error::from)
+    }
+}
+
+/// Audio damper actor. Attenuates audio level a bit.
+struct Damper {
+    next: Recipient<MixerInput>,
+}
+
+impl Actor for Damper {
+    type Error = Error;
+    type Message = Chunk;
+
+    fn name() -> &'static str {
+        "Damper"
+    }
+
+    fn handle(&mut self, _context: &Context<Self>, message: Chunk) -> Result<(), Error> {
+        // Halve the signal.
+        let chunk_slice: Arc<[Sample]> = message.iter().map(|s| [s[0] / 2, s[1] / 2]).collect();
+        let chunk: Chunk = chunk_slice.try_into().expect("sample count is correct");
+
+        // Pass it right on.
+        self.next.try_send(MixerInput::Wet(chunk)).map_err(Error::from)
+    }
+}
+
+fn main() -> Result<(), Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    let mut system = System::<NoopMetricsHandler>::new("Echo System");
+
+    // Start creating actors. Because actors "point forward", start with the last one.
+    // Set larger message channel capacity for Output actor for some cushion.
+    let output_addr = system.spawn_with_capacity(Output, 60)?;
+
+    // Create Mixer address explicitly in order to break the circular dependency loop.
+    let mixer_addr = Addr::<Mixer>::default();
+
+    // Damper feeds back into Mixer.
+    let damper_addr = system.spawn(Damper { next: mixer_addr.recipient() })?;
+
+    // Delay effect goes into Damper.
+    let delay_addr = system.spawn(Delay::new(damper_addr.recipient()))?;
+
+    // We can finally spawn the Mixer. Feeds into Output and Delay effect.
+    system.spawn_fn_with_addr(
+        move || Mixer::new(output_addr.recipient(), delay_addr.recipient()),
+        mixer_addr.clone(),
+    )?;
+
+    // Input feeds into Mixer.
+    let input_addr = system.spawn(Input { next: mixer_addr.recipient() })?;
+
+    // Kick off the pipeline.
+    input_addr.try_send(ReadNext)?;
+
+    // Let the system run, block until it finishes.
+    system.run()
+}
