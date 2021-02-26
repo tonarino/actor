4d8db074fa7d7f5c0e059270378c473aaaf0c31e
Jake McGinty <me@jake.su>
2021-02-26 14:24:22 +0900

    add slightly more useful benchmarks, more needed still

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,13 +1,14 @@
 use actor::{Actor, Addr, Context, System};
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion, Throughput};
+use std::time::Duration;
 
 struct ChainLink {
-    next: Option<Addr<ChainLink>>,
+    next: Addr<ChainLink>,
 }
 
 impl Actor for ChainLink {
     type Error = ();
-    type Message = bool;
+    type Message = u64;
 
     fn name() -> &'static str {
         "ChainLink"
@@ -18,9 +19,9 @@ impl Actor for ChainLink {
         context: &Context<Self>,
         message: Self::Message,
     ) -> Result<(), Self::Error> {
-        if let Some(ref next) = self.next {
-            next.send(message).unwrap();
-        } else if message {
+        if message > 0 {
+            self.next.send(message - 1).unwrap();
+        } else {
             context.system_handle.shutdown().unwrap();
         }
 
@@ -32,34 +33,46 @@ fn make_chain(num_actors: usize) -> (System, Addr<ChainLink>, Addr<ChainLink>) {
     let mut system = System::new("chain");
 
     let addr = Addr::default();
-    let mut next = Some(addr.clone());
+    let mut next = addr.clone();
     for _ in 0..num_actors {
-        next = Some(system.spawn(ChainLink { next }).unwrap());
+        next = system.spawn(ChainLink { next }).unwrap();
     }
 
-    (system, next.unwrap(), addr)
+    (system, next, addr)
 }
 
 fn run_chain((mut system, start, end): (System, Addr<ChainLink>, Addr<ChainLink>)) {
-    start.send(true).unwrap();
-    system.prepare(ChainLink { next: None }).with_addr(end).run_and_block().unwrap();
+    start.send(1000).unwrap();
+    system.prepare(ChainLink { next: start }).with_addr(end).run_and_block().unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("chain");
-    group.throughput(Throughput::Elements(1));
-    group.bench_function("spawn 1", |b| b.iter(|| make_chain(black_box(1))));
-    group.bench_function("spawn 10", |b| b.iter(|| make_chain(black_box(10))));
-    group.bench_function("spawn 50", |b| b.iter(|| make_chain(black_box(50))));
-    group.bench_function("chain 1", |b| {
-        b.iter_batched(|| make_chain(1), run_chain, BatchSize::SmallInput)
-    });
-    group.bench_function("chain 10", |b| {
-        b.iter_batched(|| make_chain(10), run_chain, BatchSize::SmallInput)
-    });
-    group.bench_function("chain 50", |b| {
-        b.iter_batched(|| make_chain(50), run_chain, BatchSize::SmallInput)
-    });
+    {
+        let mut spawn = c.benchmark_group("spawn");
+        spawn.throughput(Throughput::Elements(1));
+        spawn.measurement_time(Duration::from_secs(10));
+        spawn.bench_function("spawn 1", |b| b.iter(|| make_chain(black_box(1))));
+        spawn.bench_function("spawn 10", |b| b.iter(|| make_chain(black_box(10))));
+        spawn.bench_function("spawn 50", |b| b.iter(|| make_chain(black_box(50))));
+    }
+
+    {
+        let mut circular = c.benchmark_group("circular");
+        circular.measurement_time(Duration::from_secs(10));
+        circular.throughput(Throughput::Elements(1000));
+        circular.bench_function("circular (2 actors)", |b| {
+            b.iter_batched(|| make_chain(1), run_chain, BatchSize::SmallInput)
+        });
+        circular.bench_function("circular (native CPU count - 1)", |b| {
+            b.iter_batched(|| make_chain(num_cpus::get() - 2), run_chain, BatchSize::SmallInput)
+        });
+        circular.bench_function("circular (native CPU count)", |b| {
+            b.iter_batched(|| make_chain(num_cpus::get() - 1), run_chain, BatchSize::SmallInput)
+        });
+        circular.bench_function("circular (50 actors)", |b| {
+            b.iter_batched(|| make_chain(49), run_chain, BatchSize::SmallInput)
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
