7f61c33fdeee449cec87b29075409d809638be0b
Matěj Laitl <matej@laitl.cz>
2021-07-13 17:24:48 +0200

    Add Timed actor wrapper + friends for delayed/recurring messages
    See module level docs and `delay_actor.rs` example for info.
    
    Closes #14.
    
    Builds on the fact that actors can now customize their context.
    
    v2: implement Deref, take factory instead of requiring Clone.

--- examples/delay_actor.rs
+++ examples/delay_actor.rs
@@ -1,117 +1,17 @@
 use anyhow::Error;
 use env_logger::Env;
-use std::{
-    cmp::Ordering,
-    collections::BinaryHeap,
-    time::{Duration, Instant},
+use std::time::{Duration, Instant};
+use tonari_actor::{
+    timed::{RecipientExt, Timed, TimedContext},
+    Actor, System,
 };
-use tonari_actor::{Actor, Context, Recipient, SendError, System};
-
-/// Something that can be sent. A message with a recipient.
-trait Envelope {
-    fn send(self: Box<Self>) -> Result<(), SendError>;
-}
-
-impl<M> Envelope for (Recipient<M>, M) {
-    fn send(self: Box<Self>) -> Result<(), SendError> {
-        self.0.send(self.1)
-    }
-}
-
-struct DelayedMessage {
-    fire_at: Instant,
-    message: Box<dyn Envelope + Send>,
-}
-
-impl DelayedMessage {
-    fn new<M: Send + 'static>(fire_at: Instant, recipient: &Recipient<M>, message: M) -> Self {
-        Self { fire_at, message: Box::new((recipient.clone(), message)) }
-    }
-}
-
-impl PartialEq for DelayedMessage {
-    fn eq(&self, other: &Self) -> bool {
-        self.fire_at == other.fire_at
-    }
-}
-
-// We cannot derive because that would add too strict bounds.
-impl Eq for DelayedMessage {}
-
-impl PartialOrd for DelayedMessage {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        // Reverse because [BinaryHeap] is a *max* heap, but we want pop() to return lowest `fire_at`.
-        Some(self.fire_at.cmp(&other.fire_at).reverse())
-    }
-}
-
-impl Ord for DelayedMessage {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).expect("we can always compare")
-    }
-}
-
-/// A simple actor to send messages with delay. Possible extensions:
-/// * Support recurring messages.
-/// * Support cancellation.
-/// * Implement some `DelayActorExt` trait on `Recipient<DelayActor>` for caller convenience.
-struct DelayActor {
-    queue: BinaryHeap<DelayedMessage>,
-}
-
-impl DelayActor {
-    fn new() -> Self {
-        Self { queue: Default::default() }
-    }
-
-    fn schedule_timeout(&self, context: &mut Context<DelayedMessage>) {
-        // Schedule next timeout if the queue is not empty.
-        context.set_deadline(self.queue.peek().map(|earliest| earliest.fire_at));
-    }
-}
-
-impl Actor for DelayActor {
-    type Context = Context<Self::Message>;
-    type Error = Error;
-    type Message = DelayedMessage;
-
-    fn name() -> &'static str {
-        "DelayActor"
-    }
-
-    fn handle(
-        &mut self,
-        context: &mut Self::Context,
-        message: Self::Message,
-    ) -> Result<(), Self::Error> {
-        self.queue.push(message);
-        self.schedule_timeout(context);
-        Ok(())
-    }
-
-    fn deadline_passed(
-        &mut self,
-        context: &mut Self::Context,
-        _deadline: Instant,
-    ) -> Result<(), Error> {
-        // Send all messages that should have been sent by now.
-        let now = Instant::now();
-        while self.queue.peek().map(|m| m.fire_at <= now).unwrap_or(false) {
-            let message = self.queue.pop().expect("heap is non-empty, we have just peeked");
-            message.message.send()?;
-        }
-
-        self.schedule_timeout(context);
-        Ok(())
-    }
-}
 
 struct FinalConsumer {
     started_at: Instant,
 }
 
 impl Actor for FinalConsumer {
-    type Context = Context<Self::Message>;
+    type Context = TimedContext<Self::Message>;
     type Error = Error;
     type Message = String;
 
@@ -133,19 +33,19 @@ fn main() -> Result<(), Error> {
 
     let mut system = System::new("Example Timer System");
 
-    let delay_actor = system.spawn(DelayActor::new())?;
-
-    let consumer = system.spawn(FinalConsumer { started_at: Instant::now() })?.recipient();
+    let consumer =
+        system.spawn(Timed::new(FinalConsumer { started_at: Instant::now() }))?.recipient();
 
     let now = Instant::now();
-    delay_actor.send(DelayedMessage::new(now + Duration::from_secs(3), &consumer, "last"))?;
-    delay_actor.send(DelayedMessage::new(
-        now + Duration::from_secs(4),
-        &consumer,
-        "never received",
-    ))?;
-    delay_actor.send(DelayedMessage::new(now + Duration::from_secs(2), &consumer, "second"))?;
-    delay_actor.send(DelayedMessage::new(now + Duration::from_secs(1), &consumer, "first"))?;
+    consumer.send_recurring(
+        || "recurring".to_string(),
+        now + Duration::from_millis(500),
+        Duration::from_secs(1),
+    )?;
+    consumer.send_delayed("last".to_string(), Duration::from_secs(3))?;
+    consumer.send_delayed("never received".to_string(), Duration::from_secs(4))?;
+    consumer.send_timed("second".to_string(), now + Duration::from_secs(2))?;
+    consumer.send_timed("first".to_string(), now + Duration::from_secs(1))?;
 
     system.run()?;
 
---
+++ src/timed.rs
@@ -0,0 +1,220 @@
+//! Tools to make a given actor able to receive delayed and recurring messages.
+//!
+//! To apply this to a given (receiving) actor:
+//! * Use [`TimedContext<Self::Message>`] as [`Actor::Context`] associated type.
+//!   * Such actors cannot be spawned unless wrapped, making it impossible to forget wrapping it.
+//! * Wrap the actor in [`Timed`] before spawning.
+//!
+//! The wrapped actor will accept [`TimedMessage<M>`] with convenience conversion from `M`.
+//! [`RecipientExt`] becomes available for [`Recipient<TimedMessage<M>>`]s which provides methods like
+//! `send_delayed()`, `send_recurring()`.
+//!
+//! See `delay_actor.rs` example for usage.
+
+use crate::{Actor, Context, Recipient, SendError, SystemHandle};
+use std::{
+    cmp::Ordering,
+    collections::BinaryHeap,
+    ops::Deref,
+    time::{Duration, Instant},
+};
+
+/// A message that can be delivered now, at certain time and optionally repeatedly.
+pub enum TimedMessage<M> {
+    Instant { message: M },
+    Delayed { message: M, fire_at: Instant },
+    Recurring { factory: Box<dyn FnMut() -> M + Send>, fire_at: Instant, interval: Duration },
+}
+
+/// This implementation allows sending direct unwrapped messages to wrapped actors.
+impl<M> From<M> for TimedMessage<M> {
+    fn from(message: M) -> Self {
+        Self::Instant { message }
+    }
+}
+
+/// Convenience methods for [`Recipient`]s that accept [`TimedMessage`]s.
+pub trait RecipientExt<M> {
+    /// Send a `message` now. Convenience to wrap message in [`TimedMessage::Instant`].
+    fn send_now(&self, message: M) -> Result<(), SendError>;
+
+    /// Send a `message` to be delivered later at a certain instant.
+    fn send_timed(&self, message: M, fire_at: Instant) -> Result<(), SendError>;
+
+    /// Send a `message` to be delivered later after some time from now.
+    fn send_delayed(&self, message: M, delay: Duration) -> Result<(), SendError> {
+        self.send_timed(message, Instant::now() + delay)
+    }
+
+    /// Schedule sending of message at `fire_at` plus at regular `interval`s from that point on.
+    fn send_recurring(
+        &self,
+        factory: impl FnMut() -> M + Send + 'static,
+        fire_at: Instant,
+        interval: Duration,
+    ) -> Result<(), SendError>;
+}
+
+impl<M> RecipientExt<M> for Recipient<TimedMessage<M>> {
+    fn send_now(&self, message: M) -> Result<(), SendError> {
+        self.send(TimedMessage::Instant { message })
+    }
+
+    fn send_timed(&self, message: M, fire_at: Instant) -> Result<(), SendError> {
+        self.send(TimedMessage::Delayed { message, fire_at })
+    }
+
+    fn send_recurring(
+        &self,
+        factory: impl FnMut() -> M + Send + 'static,
+        fire_at: Instant,
+        interval: Duration,
+    ) -> Result<(), SendError> {
+        self.send(TimedMessage::Recurring { factory: Box::new(factory), fire_at, interval })
+    }
+}
+
+/// A [`Context`] variant available to actors wrapped by the [`Timed`] actor wrapper.
+pub struct TimedContext<M> {
+    pub system_handle: SystemHandle,
+    pub myself: Recipient<TimedMessage<M>>,
+}
+
+impl<M> TimedContext<M> {
+    fn from_context(context: &Context<TimedMessage<M>>) -> Self {
+        TimedContext {
+            system_handle: context.system_handle.clone(),
+            myself: context.myself.clone(),
+        }
+    }
+}
+
+/// A wrapper around actors to add ability to receive delayed and recurring messages.
+/// See [module documentation](self) for a complete recipe.
+pub struct Timed<A: Actor> {
+    inner: A,
+    queue: BinaryHeap<QueueItem<A::Message>>,
+}
+
+impl<M: Send + 'static, A: Actor<Context = TimedContext<M>, Message = M>> Timed<A> {
+    pub fn new(inner: A) -> Self {
+        Self { inner, queue: Default::default() }
+    }
+
+    fn schedule_timeout(&self, context: &mut <Self as Actor>::Context) {
+        // Schedule next timeout if the queue is not empty.
+        context.set_deadline(self.queue.peek().map(|earliest| earliest.fire_at));
+    }
+}
+
+impl<M: Send + 'static, A: Actor<Context = TimedContext<M>, Message = M>> Actor for Timed<A> {
+    type Context = Context<Self::Message>;
+    type Error = A::Error;
+    type Message = TimedMessage<M>;
+
+    fn handle(
+        &mut self,
+        context: &mut Self::Context,
+        timed_message: Self::Message,
+    ) -> Result<(), Self::Error> {
+        let item = match timed_message {
+            TimedMessage::Instant { message } => {
+                return self.inner.handle(&mut TimedContext::from_context(context), message);
+            },
+            TimedMessage::Delayed { message, fire_at } => {
+                QueueItem { fire_at, payload: Payload::Delayed { message } }
+            },
+            TimedMessage::Recurring { factory, fire_at, interval } => {
+                QueueItem { fire_at, payload: Payload::Recurring { factory, interval } }
+            },
+        };
+
+        self.queue.push(item);
+        self.schedule_timeout(context);
+        Ok(())
+    }
+
+    fn name() -> &'static str {
+        A::name()
+    }
+
+    fn started(&mut self, context: &mut Self::Context) {
+        self.inner.started(&mut TimedContext::from_context(context))
+    }
+
+    fn stopped(&mut self, context: &mut Self::Context) {
+        self.inner.stopped(&mut TimedContext::from_context(context))
+    }
+
+    fn deadline_passed(
+        &mut self,
+        context: &mut Self::Context,
+        _deadline: Instant,
+    ) -> Result<(), Self::Error> {
+        // Handle all messages that should have been handled by now.
+        let now = Instant::now();
+        while self.queue.peek().map(|m| m.fire_at <= now).unwrap_or(false) {
+            let item = self.queue.pop().expect("heap is non-empty, we have just peeked");
+
+            let message = match item.payload {
+                Payload::Delayed { message } => message,
+                Payload::Recurring { mut factory, interval } => {
+                    let message = factory();
+                    self.queue.push(QueueItem {
+                        fire_at: item.fire_at + interval,
+                        payload: Payload::Recurring { factory, interval },
+                    });
+                    message
+                },
+            };
+
+            // Let inner actor do its job.
+            self.inner.handle(&mut TimedContext::from_context(context), message)?;
+        }
+
+        self.schedule_timeout(context);
+        Ok(())
+    }
+}
+
+/// Access wrapped actor.
+impl<A: Actor> Deref for Timed<A> {
+    type Target = A;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+/// Implementation detail, element of message queue ordered by time to fire at.
+struct QueueItem<M> {
+    fire_at: Instant,
+    payload: Payload<M>,
+}
+
+impl<M> PartialEq for QueueItem<M> {
+    fn eq(&self, other: &Self) -> bool {
+        self.fire_at == other.fire_at
+    }
+}
+
+// We cannot derive because that would add too strict bounds.
+impl<M> Eq for QueueItem<M> {}
+
+impl<M> PartialOrd for QueueItem<M> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        // Reverse because [BinaryHeap] is a *max* heap, but we want pop() to return lowest `fire_at`.
+        Some(self.fire_at.cmp(&other.fire_at).reverse())
+    }
+}
+
+impl<M> Ord for QueueItem<M> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).expect("we can always compare")
+    }
+}
+
+enum Payload<M> {
+    Delayed { message: M },
+    Recurring { factory: Box<dyn FnMut() -> M + Send>, interval: Duration },
+}
