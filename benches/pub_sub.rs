32d7f2a0a2eebe66c560e26a73287a8ae629988f
Matěj Laitl <matej@laitl.cz>
2022-06-10 23:49:37 +0900

    Extend publisher benchmark with varying publisher, subscriber counts

--- benches/pub_sub.rs
+++ benches/pub_sub.rs
@@ -1,7 +1,7 @@
-use anyhow::{bail, Error};
+use anyhow::Error;
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::time::{Duration, Instant};
-use tonari_actor::{Actor, Addr, Context, Event, System};
+use tonari_actor::{Actor, Context, Event, Recipient, System};
 
 #[derive(Debug, Clone)]
 struct StringEvent(String);
@@ -13,6 +13,7 @@ enum PublisherMessage {
     PublishEvents,
 }
 
+#[derive(Clone)]
 struct PublisherActor {
     subscriber_count: usize,
     iterations: u64,
@@ -49,7 +50,6 @@ impl Actor for PublisherActor {
                 let elapsed = start.elapsed();
 
                 self.result_sender.try_send(elapsed)?;
-                bail!("Our work is done.");
             },
         }
 
@@ -58,12 +58,12 @@ impl Actor for PublisherActor {
 }
 
 struct SubscriberActor {
-    publisher_addr: Addr<PublisherActor>,
+    publisher_addrs: Vec<Recipient<PublisherMessage>>,
 }
 
 impl SubscriberActor {
-    fn new(publisher_addr: Addr<PublisherActor>) -> Self {
-        Self { publisher_addr }
+    fn new(publisher_addrs: Vec<Recipient<PublisherMessage>>) -> Self {
+        Self { publisher_addrs }
     }
 }
 
@@ -78,7 +78,9 @@ impl Actor for SubscriberActor {
 
     fn started(&mut self, context: &mut Self::Context) {
         context.subscribe::<StringEvent>();
-        self.publisher_addr.send(PublisherMessage::SubscriberStarted).unwrap();
+        for publisher_addr in self.publisher_addrs.iter() {
+            publisher_addr.send(PublisherMessage::SubscriberStarted).unwrap();
+        }
     }
 
     fn handle(
@@ -90,31 +92,61 @@ impl Actor for SubscriberActor {
     }
 }
 
-fn run_pubsub_system(iterations: u64) -> Duration {
-    let subscriber_count = 1;
+fn run_pubsub_system(publishers: usize, subscribers: usize, iterations: u64) -> Duration {
     let mut system = System::new("pub sub bench");
-    let (result_sender, result_receiver) = flume::bounded(1);
+    let (result_sender, result_receiver) = flume::bounded(publishers);
 
-    let publisher_actor = PublisherActor { subscriber_count, iterations, result_sender };
-    let publisher_addr = system.spawn(publisher_actor).unwrap();
-    system
-        .prepare(SubscriberActor::new(publisher_addr))
-        .with_capacity(iterations as usize)
-        .spawn()
-        .unwrap();
+    // Divide work equally. We assume that number of iterations is high and rounding doesn't matter.
+    let per_publisher_iterations = iterations / publishers as u64;
+    let publisher_actors = vec![
+        PublisherActor {
+            subscriber_count: subscribers,
+            iterations: per_publisher_iterations,
+            result_sender
+        };
+        publishers
+    ];
+    let publisher_addrs: Vec<Recipient<PublisherMessage>> = publisher_actors
+        .into_iter()
+        .map(|actor| system.prepare(actor).with_capacity(subscribers).spawn().unwrap().recipient())
+        .collect();
 
-    system.run().unwrap();
+    for _i in 0..subscribers {
+        system
+            .prepare(SubscriberActor::new(publisher_addrs.clone()))
+            .with_capacity(iterations as usize)
+            .spawn()
+            .unwrap();
+    }
 
-    result_receiver.try_recv().unwrap()
+    let mut duration_sum = Duration::default();
+    for _in in 0..publishers {
+        // This waits for all publishers to finish their work.
+        duration_sum += result_receiver.recv_timeout(Duration::from_secs(60)).unwrap();
+    }
+
+    system.shutdown().unwrap();
+
+    duration_sum
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("publish");
     group.throughput(criterion::Throughput::Elements(1));
 
-    // iter_custom() is explicitly recommended for threaded systems when measured code may live
-    // in a different thread.
-    group.bench_function("1 publisher 1 consumer", |b| b.iter_custom(run_pubsub_system));
+    for subscribers in [10, 100] {
+        for publishers in [1, 2, 10] {
+            // iter_custom() is explicitly recommended for multi-threaded systems.
+            group.bench_function(
+                format!("{publishers} publishers {subscribers} subscribers"),
+                |b| {
+                    b.iter_custom(|iterations| {
+                        run_pubsub_system(publishers, subscribers, iterations)
+                    })
+                },
+            );
+        }
+    }
 }
 
 criterion_group!(pub_sub, criterion_benchmark);
